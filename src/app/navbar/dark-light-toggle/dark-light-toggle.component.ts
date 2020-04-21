525408d2e98d82b28196fde1f38c71d25024cccb
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-04-22 00:25:44 +0200

    ending nav and dark/light modes

--- src/app/navbar/dark-light-toggle/dark-light-toggle.component.ts
+++ src/app/navbar/dark-light-toggle/dark-light-toggle.component.ts
@@ -2,32 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { GlobalVariablesService } from '../../global-variables.service';
 
 
+
 @Component({
   selector: 'app-navbar-dark-light-toggle',
-  templateUrl: './dark-light-toggle.component.html'
+  templateUrl: './dark-light-toggle.component.html',
+  styleUrls: ['./dark-light-toggle.component.css']
 })
 
 export class DarkLightToggleComponent implements OnInit {
-  // TODO make only one var that is public between components and able to change always makeItDark=True
-  light() {
-    console.log('light');
-    this.globalVariablesService.changeTheme(false);
+  makeItDark = true;
 
+  toggleTheme() {
+
+    this.makeItDark = !this.makeItDark;
+    this.globalVariablesService.changeTheme(this.makeItDark);
     console.log(this.globalVariablesService.getTheme());
-
-
-  }
-  dark() {
-    console.log('dark');
-    this.globalVariablesService.changeTheme(true);
-    console.log(this.globalVariablesService.getTheme());
-
 
   }
 
+  constructor(public globalVariablesService: GlobalVariablesService) {
 
-
-  constructor(public globalVariablesService: GlobalVariablesService) { }
+  }
 
   ngOnInit() { }
 }
