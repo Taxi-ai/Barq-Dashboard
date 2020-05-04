bc557fee517d521a415f8d9e5716d8e19d163524
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-05-04 16:55:47 +0200

    putting graph in its own component

--- src/app/users/users-graph/users-graph.component.ts
+++ src/app/users/users-graph/users-graph.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ChartDataSets } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-users-graph',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersGraphComponent implements OnInit {
 
+  lineChartData: ChartDataSets[];
+  lineChartLabels: Label[];
+  lineChartOptions: {};
+  lineChartColors: Color[];
+  lineChartLegend: boolean;
+  lineChartPlugins: [];
+  lineChartType: string;
+
+  @Input() finalCounter: number[];
+
+
   constructor() { }
 
   ngOnInit() {
+    this.handlingGraphDesign(this.finalCounter);
+  }
+
+  handlingGraphDesign(finalCounter: number[]) {
+    // function to change graph values
+    this.lineChartData = [
+      { data: finalCounter, label: 'Register Per Month' },
+    ];
+
+    this.lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.lineChartOptions = {
+      responsive: true,
+    };
+
+    this.lineChartColors = [
+      {
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,255,0,0.28)',
+      },
+    ];
+
+    this.lineChartLegend = true;
+    this.lineChartPlugins = [];
+    this.lineChartType = 'line';
+
   }
 
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user.model';
 import { UsersService } from './users.service';
-import { ChartDataSets, ChartOptions } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
-import { Route, Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -13,14 +11,7 @@ import { Route, Router, ActivatedRoute } from '@angular/router';
 export class UsersComponent implements OnInit {
 
   users: User[] = [];
-
-  lineChartData: ChartDataSets[];
-  lineChartLabels: Label[];
-  lineChartOptions: {};
-  lineChartColors: Color[];
-  lineChartLegend: boolean;
-  lineChartPlugins: [];
-  lineChartType: string;
+  finalCounter: number[] = new Array(12); // array with numbers of months duplicates
 
   constructor(private usersService: UsersService, private router: Router, private route: ActivatedRoute) { }
 
@@ -47,54 +38,23 @@ export class UsersComponent implements OnInit {
       }
     );
 
-    const finalCounter: number[] = new Array(12); // array with numbers of months duplicates
 
     months.forEach((month) => {
-      if (finalCounter[month - 1] === undefined) {
-        finalCounter[month - 1] = 1;
+      if (this.finalCounter[month - 1] === undefined) {
+        this.finalCounter[month - 1] = 1;
       } else {
-        finalCounter[month - 1] += 1;
+        this.finalCounter[month - 1] += 1;
       }
     });
 
-    for (let i = 0; i < finalCounter.length; i++) {
-      if (finalCounter[i] === undefined) {
-        finalCounter[i] = 0;
+    for (let i = 0; i < this.finalCounter.length; i++) {
+      if (this.finalCounter[i] === undefined) {
+        this.finalCounter[i] = 0;
       }
     }
-
-    console.log(finalCounter);
-
-
-    this.handlingGraphDesign(finalCounter);
-
   }
 
-  handlingGraphDesign(finalCounter: number[]) {
-    // function to change graph values
-    this.lineChartData = [
-      { data: finalCounter, label: 'Register Per Month' },
-    ];
 
-    this.lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'];
-
-    this.lineChartOptions = {
-      responsive: true,
-    };
-
-    this.lineChartColors = [
-      {
-        borderColor: 'black',
-        backgroundColor: 'rgba(255,255,0,0.28)',
-      },
-    ];
-
-    this.lineChartLegend = true;
-    this.lineChartPlugins = [];
-    this.lineChartType = 'line';
-
-  }
 
 
 }
