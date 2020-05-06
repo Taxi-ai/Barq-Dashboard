cec33d174d6c61950ca4e44dada7be7e7c37b454
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-05-06 16:13:35 +0200

    adding feedbacks routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { UsersComponent } from './users/users.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UserPageComponent } from './users/user/user-page/user-page.component';
 import { EditUserComponent } from './users/user/edit-user/edit-user.component';
+import { FeedbacksComponent } from './feedbacks/feedbacks.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'feedbacks', component: FeedbacksComponent },
   { path: 'users', component: UsersComponent },
   { path: 'users/:id', component: UserPageComponent },
   { path: 'users/:id/edit', component: EditUserComponent },
