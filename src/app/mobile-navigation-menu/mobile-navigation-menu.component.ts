74c82dc41ffa601a3d2acc15321f932e506950c2
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-06-17 03:07:05 +0200

    navigation component not appear to large screens users

--- src/app/mobile-navigation-menu/mobile-navigation-menu.component.ts
+++ src/app/mobile-navigation-menu/mobile-navigation-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-navigation-menu',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileNavigationMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+
+    if (window.innerWidth > 800) {
+      console.log(document.body.style.width);
+      console.log(window.innerWidth);
+
+
+      this.router.navigate(['./dashboard']);
+    }
+
   }
 
 }
