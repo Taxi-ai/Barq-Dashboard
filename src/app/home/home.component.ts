13a9efa65a87b53d74cdfdd6963b789ef68ff776
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-06-23 20:42:13 +0200

    finish home first item

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  dataCounters = { users: 542 };
+  usersCounters = [
+    { name: 'users', counter: 120, icon: 'fa fa-address-book' },
+    { name: 'cars', counter: 10, icon: 'fas fa-car' },
+    { name: 'partners', counter: 15, icon: 'fas fa-hands-helping' },
+    { name: 'issues', counter: 10, icon: 'fas fa-headset' }
+  ];
+  appCounters = [
+    { name: 'android', counter: 52, icon: 'fab fa-android' },
+    { name: 'ios', counter: 52, icon: 'fab fa-apple' },
+    { name: 'chrome', counter: 52, icon: 'fab fa-chrome' },
+    { name: 'firefox', counter: 52, icon: 'fab fa-firefox' }
+  ];
+  moneyCounters = [
+    { name: 'income', counter: 52, icon: 'fas fa-external-link-alt fa-rotate-90' },
+    { name: 'outcome', counter: 52, icon: 'fas fa-external-link-alt fa-rotate-180' },
+    { name: 'assets', counter: 52, icon: 'fas fa-chair' },
+    { name: 'salaries', counter: 52, icon: 'fas fa-dollar-sign' }
+  ];
 
   constructor() { }
 
