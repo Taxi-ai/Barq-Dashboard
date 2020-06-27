a1498d08730334dfc0107231d201c515a6424813
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-06-27 13:10:02 +0200

    finished all cars pages

--- src/app/cars/car/car-edit/car-edit.component.ts
+++ src/app/cars/car/car-edit/car-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from '../../car.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarsService } from '../../cars.service';
 
 @Component({
   selector: 'app-car-edit',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarEditComponent implements OnInit {
 
-  constructor() { }
+  car: Car;
+  changesIsSaved = false;
+
+  constructor(private route: ActivatedRoute, private router: Router, private carsService: CarsService) { }
 
   ngOnInit() {
+    const carID = this.route.snapshot.params.id;
+    this.carsService.getCarByID(carID).subscribe(car => {
+      // console.log(car);
+      this.car = car;
+    });
   }
+
+
+  updateCarData() {
+    console.log(this.car);
+
+    const editedCar: Car = {
+      model: this.car.model,
+      description: this.car.description,
+      color: this.car.color,
+      isDisabled: false,
+    };
+
+    if (this.car.isDisabled === 'true') { editedCar.isDisabled = true; }
+
+    this.carsService.updateCarByID(this.car._id, editedCar).subscribe(data => {
+      console.log(data);
+      this.changesIsSaved = true;
+      this.router.navigate(['../'], { relativeTo: this.route });
+    }
+    );
+
+  }
+
+
 
 }
