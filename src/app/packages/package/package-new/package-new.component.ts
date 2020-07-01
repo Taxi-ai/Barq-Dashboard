8516326da231a35100a7a43a965006249c42e459
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-07-01 13:08:57 +0200

    added new package component

--- src/app/packages/package/package-new/package-new.component.ts
+++ src/app/packages/package/package-new/package-new.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Package } from '../../package.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PackagesService } from '../../packages.service';
 
 @Component({
   selector: 'app-package-new',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PackageNewComponent implements OnInit {
 
-  constructor() { }
+  companyPackage: Package = {
+    description: '',
+    duration: 0,
+    category: 'gold',
+    price: 0,
+    limitedPricePerRide: 0,
+    numberOfGiftCodes: 0,
+    numberOfRides: 0,
+  };
+
+  spin = false;
+
+
+  constructor(private route: ActivatedRoute, private router: Router, private packagesService: PackagesService) { }
 
   ngOnInit(): void {
+
+  }
+
+
+  postNewPackage() {
+    console.log(this.companyPackage);
+    this.spin = true;
+
+    this.packagesService.postNewPackage(this.companyPackage).subscribe(data => {
+      console.log(data);
+      this.spin = false;
+
+      this.router.navigate(['../'], { relativeTo: this.route });
+    });
+
   }
 
 }
