d7dd3aceaea5ec6e884a65f870368b34d795c9d5
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-07-01 10:32:46 +0200

    finished offer page

--- src/app/offers/offer/offer-page/offer-page.component.ts
+++ src/app/offers/offer/offer-page/offer-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OffersService } from '../../offers.service';
+import { Offer } from '../../offer.model';
 
 @Component({
   selector: 'app-offer-page',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offer-page.component.css']
 })
 export class OfferPageComponent implements OnInit {
+  offer: Offer;
+  spin = true;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private offersService: OffersService) { }
 
   ngOnInit(): void {
+
+    const offerID = this.route.snapshot.params.id;
+
+    this.offersService.getOfferByID(offerID).subscribe(offer => {
+      console.log(offer);
+      this.offer = offer;
+      this.spin = false;
+
+    });
+
   }
+
+  deleteOffer() {
+    this.spin = true;
+
+    this.offersService.deleteOfferByID(this.offer._id).subscribe(data => {
+      // console.log(data);
+      this.spin = false;
+      this.router.navigate(['../'], { relativeTo: this.route });
+    },
+      error => {
+        this.spin = false;
+      });
+  }
+
+
 
 }
