0e6e2d0f3f5e9f0134e4fbfc4abda6772f9a2291
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-07-01 13:18:31 +0200

    finished all packages components

--- src/app/packages/package/package-edit/package-edit.component.ts
+++ src/app/packages/package/package-edit/package-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Package } from '../../package.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PackagesService } from '../../packages.service';
 
 @Component({
   selector: 'app-package-edit',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PackageEditComponent implements OnInit {
 
-  constructor() { }
+  companyPackage: Package;
+  spin = true;
+
+  constructor(private route: ActivatedRoute, private router: Router, private packagesService: PackagesService) { }
 
   ngOnInit() {
+    const companyPackageID = this.route.snapshot.params.id;
+    this.packagesService.getPackageByID(companyPackageID).subscribe(companyPackage => {
+      // console.log(companyPackage);
+      this.companyPackage = companyPackage;
+      this.spin = false;
+    });
+  }
+
+
+  updatePackageData() {
+
+    this.spin = true;
+    const editedCompanyPackage: Package = {
+      description: this.companyPackage.description,
+      duration: this.companyPackage.duration,
+      category: this.companyPackage.category,
+      price: this.companyPackage.price,
+      numberOfGiftCodes: this.companyPackage.numberOfGiftCodes,
+      numberOfRides: this.companyPackage.numberOfRides,
+      limitedPricePerRide: this.companyPackage.limitedPricePerRide
+    };
+
+    this.packagesService.updatePackageByID(this.companyPackage._id, editedCompanyPackage).subscribe(data => {
+      console.log(data);
+      this.spin = false;
+      this.router.navigate(['../'], { relativeTo: this.route });
+    }
+    );
+
+
+
   }
 
 }
