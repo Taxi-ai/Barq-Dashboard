911a86b6baa0d656c96fdf55b1a6094702778324
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-07-14 21:52:26 +0200

    added code to prevent CORS problem but it's commented

--- src/app/admin-login/auth-interceptor.service.ts
+++ src/app/admin-login/auth-interceptor.service.ts
@@ -30,3 +30,12 @@ export class AuthInterceptorService implements HttpInterceptor {
   }
 
 }
+
+
+// ----> To solve CORS problem <----\\
+// const proxyurl = "https://cors-anywhere.herokuapp.com/";
+// const url = "https://example.com"; // site that doesn’t send Access-Control-*
+// fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+// .then(response => response.text())
+// .then(contents => console.log(contents))
+// .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
