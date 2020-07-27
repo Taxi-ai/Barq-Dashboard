727e9f20470dbd87527970e7e57cb2a8d5905af5
Mahmoud Ibn Samy <mahmoudsamy50@outlook.com>
2020-07-27 02:36:05 +0200

    updating site map in powerpoint file

--- src/app/admin-login/auth-interceptor.service.ts
+++ src/app/admin-login/auth-interceptor.service.ts
@@ -10,8 +10,10 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
 
+
     if (req.url === 'https://barq-api.azurewebsites.net/api/adminAuth') {
       console.log('admin-auth request');
+
       return next.handle(req);
     }
 
@@ -22,7 +24,7 @@ export class AuthInterceptorService implements HttpInterceptor {
           return next.handle(req);
         }
         const modifiedReq = req.clone({
-          headers: new HttpHeaders().append('x-auth-token', admin.jwtToken)
+          headers: new HttpHeaders().append('x-auth-token', admin.jwtToken),
         });
         return next.handle(modifiedReq);
       }));
@@ -30,12 +32,3 @@ export class AuthInterceptorService implements HttpInterceptor {
   }
 
 }
-
-
-// ----> To solve CORS problem <----\\
-// const proxyurl = "https://cors-anywhere.herokuapp.com/";
-// const url = "https://example.com"; // site that doesn’t send Access-Control-*
-// fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-// .then(response => response.text())
-// .then(contents => console.log(contents))
-// .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
