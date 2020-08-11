b089a8add60cd5ee0a9e1395da19a67569d319ce
mahmoud-ibn-samy <mahmoudsamy50@outlook.com>
2020-08-11 18:36:10 +0200

    changing address bar color in mobile browsers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { Meta, BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './admin-login/auth-interceptor.service';
 import { SharedModule } from './shared/shared.module';
@@ -25,7 +28,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }, Meta],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/theme.service.ts
+++ src/app/dashboard/theme.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Injectable({ providedIn: 'root' })
 
 export class ThemeService {
+
+  constructor(private metaService: Meta) { }
 
   private makeItDark: boolean; // theme value, true -> dark & false -> light
 
@@ -18,14 +21,42 @@ export class ThemeService {
 
     // if user needs light them set attribute of 'dashboard.css' file to be light and same with dark
     if (this.makeItDark) {
+
+      this.updatingAddressBarColor(true);
+
       document.documentElement.setAttribute('data-theme', 'dark'); // set attribute and change css
+
       localStorage.setItem('theme', 'dark'); // save new theme in localStorage so we use in next visit
+
     } else {
 
+      this.updatingAddressBarColor(false);
+
       document.documentElement.setAttribute('data-theme', 'light'); // set attribute and change css
+
       localStorage.setItem('theme', 'light'); // save new theme in localStorage so we use in next visit
+
     }
   }
 
+
+  updatingAddressBarColor(makeItDark: boolean) {
+
+    let barColor: { hex: string; text: string; };
+    if (makeItDark) {
+      barColor = { hex: '#050510', text: 'black' };
+    } else {
+      barColor = { hex: '#f6f7fb', text: 'default' };
+    }
+
+    this.metaService.updateTag({ name: 'theme-color', content: barColor.hex }, 'name=\'theme-color\'');
+
+    // tslint:disable-next-line: max-line-length
+    this.metaService.updateTag({ name: 'apple-mobile-web-app-status-bar-style', content: barColor.text }, 'name=\'apple-mobile-web-app-status-bar-style\'');
+
+    // tslint:disable-next-line: max-line-length
+    this.metaService.updateTag({ name: 'msapplication-navbutton-color', content: barColor.hex }, 'name=\'msapplication-navbutton-color\'');
+
+  }
 
 }
