64e48fd568bda47ad600516007f9e3d3670c5457
mahmoud-ibn-samy <mahmoudsamy50@outlook.com>
2020-08-14 17:21:38 +0200

    finished edit admin page

--- src/app/admins/admin/admin-edit/admin-edit.component.ts
+++ src/app/admins/admin/admin-edit/admin-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AdminsService } from '../../admins.service';
+import { Admin } from '../../admin.model';
 
 @Component({
   selector: 'app-admin-edit',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminEditComponent implements OnInit {
 
-  constructor() { }
+  admin: Admin;
+  spin = true;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private router: Router, private adminsService: AdminsService) { }
+
+  ngOnInit() {
+    const adminID = this.route.snapshot.params.id;
+    this.adminsService.getAdminByID(adminID).subscribe(admin => {
+      // console.log(admin);
+      this.admin = admin;
+      this.spin = false;
+
+    });
+
   }
 
+  updateAdminData() {
+    this.spin = true;
+
+    const editedAdmin: Admin = {
+      username: this.admin.username,
+      email: this.admin.email,
+      dateOfBirth: this.admin.dateOfBirth,
+      address: { country: this.admin.address.country, city: this.admin.address.city, street: this.admin.address.street },
+      image: this.admin.image,
+      phone: this.admin.phone
+    };
+
+    this.adminsService.updateAdminByID(this.admin._id, editedAdmin).subscribe(data => {
+      console.log(data);
+      this.spin = false;
+      this.router.navigate(['../'], { relativeTo: this.route });
+    }
+      , error => {
+        this.spin = false;
+      });
+
+  }
 }
